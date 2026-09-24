3ca055d41bafeec3c406580524f8a34861103cca
Hudson Sheng <hudsonsheng@gmail.com>
2026-09-23 21:09:54 -0400

    Updated meeting minutes template file name

--- meetings/generate_meeding_file.sh
+++ meetings/generate_meeding_file.sh
@@ -5,7 +5,7 @@ FILENAME="${DATE}_meeting_minutes.md"
 
 if [ ! -f $FILENAME ]; then
    echo "Creating file $FILENAME"
-   cat 2026_mm_dd_meeting_minutes_template.md > "$FILENAME"
+   cat meeting_minutes_template.md > "$FILENAME"
 else
    echo "File $FILENAME already exists"
 fi
