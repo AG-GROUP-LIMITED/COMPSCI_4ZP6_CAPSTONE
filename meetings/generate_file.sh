67fa9e7c9bc82b8a42726513e8c41362ee376ce4
Hudson Sheng <hudsonsheng@gmail.com>
2026-09-23 21:02:02 -0400

    Modified meeting generation file to not overwrite and copy template

--- meetings/generate_file.sh
+++ meetings/generate_file.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 DATE=$(date +"%Y_%m_%d")
-touch "$DATE"_meeting_minutes.md
+FILENAME="${DATE}_meeting_minutes.md"
+
+if [ ! -f $FILENAME ]; then
+   echo "Creating file $FILENAME"
+   cat 2026_mm_dd_meeting_minutes_template.md > "$FILENAME"
+else
+   echo "File $FILENAME already exists"
+fi
+
